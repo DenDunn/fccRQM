01663397e0eb20be2506e6f32fb2fa57ae7b62c1
deni zaikin <deni zaikin>
2016-10-30 22:16:35 +0300

    twitter posting is done.

--- qlogic.js
+++ qlogic.js
@@ -2,12 +2,16 @@ $(document).ready(function() {
     $("#QuoteBtn").on("click", function(){
         $.ajax({
             url: "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",
+            //url: "https://jsonp.afeld.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",
             dataType: "json",
             cache: false,
             type: "GET",
             success: function (json) {
+                var author = "unknown";
                 $("#qoutePlace").html(json.quoteText);
-                $("#authorPlace").html(json.quoteAuthor);
+                if (json.quoteAuthor !== "") author = json.quoteAuthor;
+                $("#authorPlace").html(author);
+                $("#twitterious").attr("href", "https://twitter.com/intent/tweet?text=" + json.quoteText + author + "&hashtags=quotes");
             }
         })
     });
